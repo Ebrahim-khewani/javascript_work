817c430f51c091967d8b6054259fcb766c130946
Ebrahim-khewani <eng1.ebrahim@gmail.com>
2023-10-03 17:09:27 +0300

    search function

--- js/function.js
+++ js/function.js
@@ -1,8 +1,6 @@
 
-var x;
-x = 0;
 
-var fruits = [
+var itemofproducts = [
     {
         addres: "address",
         dtl: "dtl",
@@ -23,25 +21,58 @@ var fruits = [
         img: " "
     }
 ];
-fruits.forEach(myFunction => {
-    x += myFunction.price;
+
+var totalofprice = 0;
+
+itemofproducts.forEach(functionTotalprod => {
+    totalofprice += functionTotalprod.price;
 }
 );
 
-console.log(x);
+console.log(totalofprice);
 
-itms = [];
-
-function check(num) {
-    fruits.forEach(prou => {
-        if (prou.addres == num)
-            itms.push(prou);
+itemsfound = [];
+let itemserch = function (txtserch) {
+    itemsfound.splice(0, itemsfound.length);
+    itemofproducts.forEach(product => {
+        // if (product.addres === se)
+         if (product.addres === txtserch)
+        itemsfound.push(product);
     });
-    return itms;
+ 
+    if (itemsfound === null || itemsfound.length === 0) {
+        // console.log("Item not Found");
+        
+       document.getElementById("results").innerHTML  = "Not found"+' '+txtserch;
+         //console.log(se);
+      } else {
+        document.getElementById("results").innerHTML  = "found"+' '+txtserch;
+        //console.log('found'+ se);
+      }
+     
+    // return itemsfound;
 }
 
-console.log(check("address3"));
+//console.log(itemserch("address3"));
 
 
+const btnserch =   document.getElementById('btn-serch');
 
+var inputserc = document.querySelector('#serch')
 
+btnserch.addEventListener('click', ()=>{
+    
+    itemserch(inputserc.value);
+});
+
+//document.getElementById("demo").innerHTML = document.getElementById('serch').value;
+//  const element = document.getElementById("myBtn");
+//   element.addEventListener("click", myFunction);
+//  function myFunction() {
+//    document.getElementById("demo").innerHTML = document.getElementById('serch').value;
+// }
+//  document.addEventListener('DOMContentLoaded', function() {
+//      var button = document.querySelector('#btn-serch');
+//      button.addEventListener('click', itemserch(document.querySelector('#serch')));
+//    });
+//  }
